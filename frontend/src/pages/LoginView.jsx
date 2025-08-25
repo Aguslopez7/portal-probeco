453162b22e9e609303edf3a753c62255cf3f0476
angeloti19 <agiroldi45@gmail.com>
2025-08-25 14:59:41 -0300

    Modify SecurityConfig and LoginView

--- frontend/src/pages/LoginView.jsx
+++ frontend/src/pages/LoginView.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import Logo from '../assets/grupo-planeta-logo.jpeg';
+import Logo from '../assets/Logo-ProBeCo-Color.png';
 import axios from 'axios'; // Assuming you're using axios
 
 const LoginView = () => {
